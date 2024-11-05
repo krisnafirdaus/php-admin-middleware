190ee2e1cb3a8c25c948bc87be72c95d26301a08
Krisna Firdaus <krisnafirdaus@gmail.com>
2024-11-05 21:08:58 +0700

    initial project

---
+++ component/navbar.php
@@ -0,0 +1,7 @@
+<nav class="top-menu">
+    <ul>
+        <li><a href="dashboard.php">Dashboard</a></li>
+        <li><a href="profile.php">Profile</a></li>
+        <li><a href="../logout.php">Logout</a></li>
+    </ul>
+</nav>
